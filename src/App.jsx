7c618be516e108baa1f742c1318acd4393e2eed1
jordam mendes <jordammendes@gmail.com>
2022-10-05 15:16:35 -0300

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import Cards from "./components/Cards";
+import elections from './image/elections.png'
 
 function App() {
   const [votes, setVotes] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -9,44 +12,21 @@ function App() {
         "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json";
       const response = await fetch(URL);
       const data = await response.json();
+      setData(data);
       setVotes(data.cand);
     };
     fetchData();
   }, []);
-  console.log(votes);
+  console.log(data);
   return (
-    <table>
-      <thead>
-        {votes.length > 0 && <tr>
-          <th>{votes[0].nm}</th>
-          <th>{votes[1].nm}</th>
-          <th>{votes[2].nm}</th>
-          <th>{votes[3].nm}</th>
-          <th>{votes[4].nm}</th>
-          <th>{votes[5].nm}</th>
-          <th>{votes[6].nm}</th>
-          <th>{votes[7].nm}</th>
-          <th>{votes[8].nm}</th>
-          <th>{votes[9].nm}</th>
-          <th>{votes[10].nm}</th>
-        </tr>}
-      </thead>
-      <tbody>
-        {votes.length > 0 && <tr>
-          <td>{`Total de votos: ${votes[0].vap} // Percentual: ${votes[0].pvap}`}</td>
-          <td>{`Total de votos: ${votes[1].vap} // Percentual: ${votes[1].pvap}`}</td>
-          <td>{`Total de votos: ${votes[2].vap} // Percentual: ${votes[2].pvap}`}</td>
-          <td>{`Total de votos: ${votes[3].vap} // Percentual: ${votes[3].pvap}`}</td>
-          <td>{`Total de votos: ${votes[4].vap} // Percentual: ${votes[4].pvap}`}</td>
-          <td>{`Total de votos: ${votes[5].vap} // Percentual: ${votes[5].pvap}`}</td>
-          <td>{`Total de votos: ${votes[6].vap} // Percentual: ${votes[6].pvap}`}</td>
-          <td>{`Total de votos: ${votes[7].vap} // Percentual: ${votes[7].pvap}`}</td>
-          <td>{`Total de votos: ${votes[8].vap} // Percentual: ${votes[8].pvap}`}</td>
-          <td>{`Total de votos: ${votes[9].vap} // Percentual: ${votes[9].pvap}`}</td>
-          <td>{`Total de votos: ${votes[10].vap} // Percentual: ${votes[10].pvap}`}</td>
-        </tr>}
-      </tbody>
-    </table>
+    <div>
+      <header>
+        <img src={ elections } alt={ elections } width="200px" />
+        <span>{`${data.pvv}% das seções totalizadas`}</span>
+        <span>{`Última atualização ${data.dt} ${data.ht}`}</span>
+      </header>
+      <Cards candidatos={votes}/>
+    </div>
   );
 }
 
---
+++ src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Cards({ candidatos }) {
+  return (
+    <div className="circulo">
+      {candidatos.map((cand) => (
+        <div className="circulos" key={cand.nm}>
+          <div className={cand.nm} />
+          <h1>{cand.nm.replace(/&apos;/, "'")}</h1>
+          <div className="votes">
+            <span>{`Votos: ${(+cand.vap).toLocaleString(
+              "pt-BR"
+            )}`}</span>
+            <h2>{`${cand.pvap}%`}</h2>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Cards;
